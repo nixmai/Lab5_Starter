da2ff007bc03c27076156a8e2903d031017570a6
nixmai <nkasibatla@ucsd.edu>
2025-05-05 16:34:13 -0700

    test: breaking on purpose

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,7 +2,7 @@
 import { sum } from '../code-to-unit-test/sum.js';
 
 test('adds 1 + 2 to equal 3 (direct)', () => {
-  expect(1 + 2).toBe(3); // basic arithmetic test
+  expect(1 + 2).toBe(4); // basic arithmetic test
 });
 
 test('adds 1 + 2 using sum() to equal 3', () => {
