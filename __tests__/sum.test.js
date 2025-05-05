0c112d8c30645ae358752bc22bfed384be3c720d
nixmai <nkasibatla@ucsd.edu>
2025-05-05 15:49:06 -0700

    sum test complete

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,5 +1,10 @@
 // sum.test.js
+import { sum } from '../code-to-unit-test/sum.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  // TODO
+test('adds 1 + 2 to equal 3 (direct)', () => {
+  expect(1 + 2).toBe(3); // basic arithmetic test
+});
+
+test('adds 1 + 2 using sum() to equal 3', () => {
+  expect(sum(1, 2)).toBe(3); // tests your actual function
 });
